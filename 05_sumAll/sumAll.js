a1302858d20aab88d35b7eae02bd51cc39523884
gdmich <59811223+gdmich@users.noreply.github.com>
2025-04-30 16:33:09 +0300

    Add function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,31 @@
-const sumAll = function() {
+const sumAll = function(lower, upper) {
+
+    if(lower < 0 ||typeof lower != "number" || typeof upper != "number" || !Number.isInteger(lower) || !Number.isInteger(upper)){
+
+        return "ERROR";
+    }
+
+    let sum = 0;
+    
+    if(lower < upper){
+
+        for(lower; lower <= upper; lower++){
+
+            sum += lower;
+    
+        }
+
+    }
+    else{
+
+        for(upper; upper <= lower; upper++){
+
+            sum += upper;
+        }
+    }
+
+
+    return sum;
 
 };
 
