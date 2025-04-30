c4e494f593ab8b905c0c35e8b51397a6be62eb53
gdmich <59811223+gdmich@users.noreply.github.com>
2025-04-30 15:53:55 +0300

    Add reverse

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(phrase) {
 
+    let reversed = "";
+    let i = phrase.length-1;
+    
+    for(i; i > -1; i--){
+
+        reversed += phrase[i];
+
+    }
+
+    return reversed;
 };
 
 // Do not edit below this line
